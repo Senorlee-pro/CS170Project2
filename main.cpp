2c76507fb940f88f2aa520cb95dead0d9ba8b574
Senorlee <senorlee624@gmail.com>
2025-12-11 18:27:15 -0800

    Add results as comments

--- main.cpp
+++ main.cpp
@@ -5,6 +5,15 @@
 
 using namespace std;
 
+// Group: Jiahan Li - jli1130 - session 022, Bestin Watts - bwatt004 - session 022, Yangao Yi - yyi029 - session 021
+// DatasetID: "small.txt" "large.txt"
+// Small Dataset Results:
+//      Forward: Feature Subset: {5,3}  Acc: 92%
+//      Backward: Feature Subset: {2,4,5,7,10}  Acc: 82%
+// Large Dataset Results:
+//      Forward: Feature Subset: {27,1}  Acc: 95.5%
+//      Backward: Feature Subset: {2,3,4,5,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40}  Acc: 72.2%
+
 void read(string filename, vector<int>& labels, vector<vector<double>>& features);
 void forward_selection(int feature_num, double initial_score);
 void backward_elimination(int feature_num, double initial_score);
